224f213058c1a4883654c7eb406b4b9df0514701
Ezzerof <christian.bitca@gmail.com>
2025-05-11 21:59:18 +0100

    feat: implement GET, PUT, DELETE endpoints for Client entity and add latest version of Lombok in pom

--- src/main/java/com/smartinvoice/client/controller/ClientController.java
+++ src/main/java/com/smartinvoice/client/controller/ClientController.java
@@ -5,19 +5,56 @@ import com.smartinvoice.client.dto.ClientResponseDto;
 import com.smartinvoice.client.service.ClientService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/clients")
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class ClientController {
 
     private final ClientService clientService;
 
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @Autowired
+
+
+    // Create a new client
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ClientResponseDto create(@Valid @RequestBody ClientRequestDto dto) {
         return clientService.createClient(dto);
+    }
+
+    // Get a list of all clients
+    @GetMapping
+    public List<ClientResponseDto> getAllClients() {
+        return clientService.getAllClients();
+    }
+
+    // Get a single client by ID
+    @GetMapping("/{id}")
+    public ClientResponseDto getClientById(@PathVariable Long id) {
+        return clientService.getClientById(id);
+    }
+
+    // Update client information
+    @PutMapping("/{id}")
+    public ClientResponseDto updateClient(@PathVariable Long id, @Valid @RequestBody ClientRequestDto dto) {
+        return clientService.updateClient(id, dto);
+    }
+
+    // Delete a client by ID
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteClient(@PathVariable Long id) {
+        clientService.deleteClient(id);
     }
 }
