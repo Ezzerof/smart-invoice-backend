558892c23fbac902171525376fd2d619478a3ef3
Ezzerof <christian.bitca@gmail.com>
2025-06-19 22:41:49 +0100

    fix: Removed unused InvoiceRepository from the InvoiceController

--- src/main/java/com/smartinvoice/invoice/controller/InvoiceController.java
+++ src/main/java/com/smartinvoice/invoice/controller/InvoiceController.java
@@ -20,7 +20,6 @@ import java.util.List;
 public class InvoiceController {
 
     private final InvoiceService invoiceService;
-    private final InvoiceRepository invoiceRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
